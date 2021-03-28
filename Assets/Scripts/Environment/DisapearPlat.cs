adde9e12284ecb4dfd7c9d1698d72a2687f11dd1
s20884 <s20884@pjwstk.edu.pl>
2021-03-28 15:33:50 +0200

    PracaDomowa2

---
+++ Assets/Scripts/Environment/DisapearPlat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisapearPlat : MonoBehaviour
+{
+    IEnumerator OnCollisionEnter2D(Collision2D coll)
+    {
+
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+
+    }
+}
