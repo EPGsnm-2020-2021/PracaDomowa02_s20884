e5df4ce0bd2baac7ec1714d834807e050fd13a1f
s20884 <s20884@pjwstk.edu.pl>
2021-04-06 20:58:44 +0200

    PracaDomowa3 - Animator

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+   
+    
+    float curHealth;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        curHealth = 10;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerDeath();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Kałuza")
+        {
+            curHealth = 0;
+        }
+    }
+   void PlayerDeath()
+    {
+        if (curHealth <= 0)
+        {
+            animator.SetBool("IsDead", true);
+            
+        }
+    }
+    private void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,13 +9,15 @@ public class PlayerMovement : MonoBehaviour
     
 
     private Rigidbody2D rb;
-    private bool isGrounded;
-
+    public bool isGrounded;
+    private Animator animator;
+    public float Dist;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        isGrounded = true;
+        animator = GetComponent<Animator>();
+        animator.SetBool("IsDead", false);
     }
 
     // Update is called once per frame
@@ -23,11 +25,12 @@ public class PlayerMovement : MonoBehaviour
     {
         float xDisplacement = Input.GetAxis("Horizontal") * speed;
         rb.velocity = new Vector2(xDisplacement, rb.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(new Vector2(0, jumpForce));
-            isGrounded = false;
+            
         }
 
 
@@ -39,8 +42,31 @@ public class PlayerMovement : MonoBehaviour
         {
             speed = 10;
         }
-     }
+
+        Debug.DrawLine(transform.position, transform.position + new Vector3(0, -Dist, 0), Color.green);
+
+        var Hits2D = Physics2D.RaycastAll(transform.position, -transform.up, Dist);
+        foreach (var item in Hits2D)
+        {
+            if (item.transform.tag == "Ground")
+            {
+                isGrounded = true;
+                animator.SetBool("IsJumping", false);
+            }else if(item.transform.tag == "Death")
+            {
+                animator.SetBool("IsDead", true);
+            }
+
+            else
+            {
+                isGrounded = false;
+                animator.SetBool("IsJumping", true);
+            }
+
+        }
+    }
     
+ 
 
     void OnCollisionEnter2D(Collision2D col)
     {
