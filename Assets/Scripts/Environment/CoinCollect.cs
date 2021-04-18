abdffb67ba1c1ad87cd5a5cd6e11fdcf5948d29f
s20884 <s20884@pjwstk.edu.pl>
2021-04-18 20:50:00 +0200

    Zadanie4

---
+++ Assets/Scripts/Environment/CoinCollect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour
+{
+   public Animator animator;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.ResetTrigger("CoinGet");
+        
+    }
+
+    void OnCollisionEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            animator.SetTrigger("CoinGet");
+        }
+    }
+}
+ 
